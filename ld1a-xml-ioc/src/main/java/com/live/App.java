76f5b9cf8ef93a056d7cd9a80621dc74f54e7f44
Umang Agrawal <umang_agl@hotmail.com>
2020-07-14 20:50:19 -0500

    .

--- ld1a-xml-ioc/src/main/java/com/live/App.java
+++ ld1a-xml-ioc/src/main/java/com/live/App.java
@@ -9,25 +9,48 @@ import com.live.service.ProductServiceImpl;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Enterprise Application Starter");
-        //ProductRepository productRepository = new OracleRepository();
-        ProductRepository productRepository = null;
-        
-        //Load the spring container
-        //spring container uses applicationContext.xml to configure the spring beans
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        
-        //access the spring bean from the application for Oracle
-        productRepository = (ProductRepository) context.getBean("productRepository");
-        productRepository.getProducts().forEach(System.out::println);
-        
-        //access the spring bean from the application for MySql
-        productRepository = (ProductRepository) context.getBean("productRepository1");
-        productRepository.getProducts().forEach(System.out::println);
-        
-        
-        context.close();
-    }
+	public static void main( String[] args )
+	{
+		performIOC();
+		testSingleton();
+	}
+
+	private static void performIOC() {
+		System.out.println( "Enterprise Application Starter");
+		//ProductRepository productRepository = new OracleRepository();
+		ProductRepository productRepository = null;
+
+		//Load the spring container
+		//spring container uses applicationContext.xml to configure the spring beans
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		//access the spring bean from the application for Oracle
+		productRepository = (ProductRepository) context.getBean("productRepository");
+		productRepository.getProducts().forEach(System.out::println);
+
+		ProductRepository mySqlRepository = null;
+		//access the spring bean from the application for MySql
+		mySqlRepository = context.getBean("mySqlRepository",ProductRepository.class);
+		mySqlRepository.getProducts().forEach(System.out::println);
+
+		//Testing if its a singleton
+		System.out.println(productRepository);
+		System.out.println(mySqlRepository);
+
+		context.close();
+	}
+
+	private static void testSingleton() {
+
+		ProductRepository productRepository = null;
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		productRepository = (ProductRepository) context.getBean("productRepository");
+		System.out.println(productRepository);
+
+		productRepository = (ProductRepository) context.getBean("productRepository");
+		System.out.println(productRepository);
+
+
+	}
 }
