fff848833dde55cf8cd8f6b0b1622a737ca72e7c
Umang Agrawal <umang_agl@hotmail.com>
2020-07-17 20:23:19 -0500

    .

--- ld1a-crud-spring-mvc-hib-nomvn/src/com/live/controller/ProductController.java
+++ ld1a-crud-spring-mvc-hib-nomvn/src/com/live/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.live.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
-	
+@GetMapping("value=/list")	
+	public String getProducts() {
+		return "products";
+	}
 }
